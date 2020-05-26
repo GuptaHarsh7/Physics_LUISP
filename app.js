5324d2f207e0b451acd97f5c11146cf456a9d166
Harsh Gupta <harshgupta6641@gmail.com>
2020-05-26 12:14:34 +0000

    faculty. facility, and contact page

--- app.js
+++ app.js
@@ -7,8 +7,28 @@ app.set('view engine','ejs');
 app.use(express.static(__dirname + "/public"));
 
 app.get("/",function(req,res){
-    console.log("OK");
-})
+    res.render("home");
+});
+
+app.get("/faculty",function(req,res){
+    res.render("faculty");
+});
+
+app.get("/contact",function(req,res){
+    res.render("contact");
+});
+
+app.get("/Facilities",function(req,res){
+    res.render("facilities");
+});
+
+app.get("/lectures",function(req,res){
+    res.render("lectures");
+});
+
+app.get("/resources",function(req,res){
+    res.render("resources");
+});
 
 app.listen(process.env.PORT,process.env.IP,function(){
     console.log("Connected");
