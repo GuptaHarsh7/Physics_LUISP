0ab1596e211d7704e7473bebec5940327a5bad91
Harsh Gupta <harshgupta6641@gmail.com>
2020-05-28 16:40:34 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-var express =require("express"),
-    bodyParser= require("body-parser"),
-    app = express();
+var express =require("express");
+var bodyParser= require("body-parser");
+var app = express();
 
 app.use(bodyParser.urlencoded({extended:true}));
 app.set('view engine','ejs');
